4e5a4e2f92b8af68fbc84e5bfc3904475826f13f
Christopher Biscardi <chris@christopherbiscardi.com>
2022-04-26 04:14:37 -0700

    gameover

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
-use board::{Position, SpawnSnakeSegment};
+use board::{Board, Position, SpawnSnakeSegment};
+use controls::Direction::*;
 use food::{Food, NewFoodEvent};
+use iyes_loopless::state::NextState;
 use snake::Snake;
 
 pub mod board;
@@ -15,6 +17,13 @@ pub enum GameState {
     Playing,
 }
 
+#[derive(PartialEq, Eq, Debug)]
+enum GameOverReason {
+    HitWall,
+    HitSnake,
+    Win,
+}
+
 pub fn tick(
     mut commands: Commands,
     mut snake: ResMut<Snake>,
@@ -22,48 +31,96 @@ pub fn tick(
     input: Res<controls::Direction>,
     query_food: Query<(Entity, &Position), With<Food>>,
     mut food_events: EventWriter<NewFoodEvent>,
+    query_board: Query<&Board>,
 ) {
+    let board = query_board.single();
+
     let mut next_position = snake.segments[0].clone();
-    match *input {
-        controls::Direction::Up => {
+    let hit_wall = match *input {
+        Up if next_position.y == board.size - 1 => {
+            Some(GameOverReason::HitWall)
+        }
+        Up => {
             next_position.y += 1;
+            None
         }
-        controls::Direction::Down => {
+        Down if next_position.y == 0 => {
+            Some(GameOverReason::HitWall)
+        }
+        Down => {
             next_position.y -= 1;
+            None
         }
-        controls::Direction::Right => {
+        Right if next_position.x == board.size - 1 => {
+            Some(GameOverReason::HitWall)
+        }
+        Right => {
             next_position.x += 1;
+            None
         }
-        controls::Direction::Left => {
+        Left if next_position.x == 0 => {
+            Some(GameOverReason::HitWall)
+        }
+        Left => {
             next_position.x -= 1;
+            None
         }
     };
-    snake.segments.push_front(next_position);
-    commands.add({
-        SpawnSnakeSegment {
-            position: next_position,
-        }
-    });
 
-    // remove old snake segment, unless snake just
-    // ate food
-    let is_food = query_food
-        .iter()
-        .find(|(_, pos)| &&next_position == pos);
+    let hit_self =
+        if snake.segments.contains(&next_position) {
+            Some(GameOverReason::HitSnake)
+        } else {
+            None
+        };
 
-    match is_food {
-        Some((entity, _)) => {
-            commands.entity(entity).despawn_recursive();
-            food_events.send(NewFoodEvent);
+    let has_won = if snake.segments.len()
+        == board.size as usize * board.size as usize
+    {
+        Some(GameOverReason::Win)
+    } else {
+        None
+    };
+    match hit_wall.or(hit_self).or(has_won) {
+        Some(GameOverReason::HitWall)
+        | Some(GameOverReason::HitSnake)
+        | Some(GameOverReason::Win) => {
+            commands.insert_resource(NextState(
+                GameState::Menu,
+            ));
         }
         None => {
-            let old_tail =
-                snake.segments.pop_back().unwrap();
-            if let Some((entity, _)) = positions
+            snake.segments.push_front(next_position);
+            commands.add({
+                SpawnSnakeSegment {
+                    position: next_position,
+                }
+            });
+
+            // remove old snake segment, unless snake just
+            // ate food
+            let is_food = query_food
                 .iter()
-                .find(|(_, pos)| pos == &&old_tail)
-            {
-                commands.entity(entity).despawn_recursive();
+                .find(|(_, pos)| &&next_position == pos);
+            match is_food {
+                Some((entity, _)) => {
+                    commands
+                        .entity(entity)
+                        .despawn_recursive();
+                    food_events.send(NewFoodEvent);
+                }
+                None => {
+                    let old_tail =
+                        snake.segments.pop_back().unwrap();
+                    if let Some((entity, _)) = positions
+                        .iter()
+                        .find(|(_, pos)| pos == &&old_tail)
+                    {
+                        commands
+                            .entity(entity)
+                            .despawn_recursive();
+                    }
+                }
             }
         }
     }
