a5f74884c3af31d9d61eedf1d6d90f00bcc536b1
Christopher Biscardi <chris@christopherbiscardi.com>
2022-04-26 03:03:27 -0700

    tick

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+use bevy::prelude::*;
+use board::Position;
+use snake::Snake;
+
 pub mod board;
 pub mod colors;
 pub mod snake;
+
+pub fn tick(
+    mut commands: Commands,
+    mut snake: ResMut<Snake>,
+    positions: Query<(Entity, &Position)>,
+) {
+    let mut next_position = snake.segments[0].clone();
+    next_position.x += 1;
+    snake.segments.push_front(next_position);
+    let old_tail = snake.segments.pop_back().unwrap();
+    if let Some((entity, _)) =
+        positions.iter().find(|(_, pos)| pos == &&old_tail)
+    {
+        commands.entity(entity).despawn_recursive();
+    }
+}
