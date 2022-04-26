d9cda780c4d2dc53a1f88f61c3fe5d57b306e8d8
Christopher Biscardi <chris@christopherbiscardi.com>
2022-04-26 03:33:19 -0700

    move

---
+++ src/controls.rs
@@ -0,0 +1,39 @@
+use bevy::prelude::*;
+
+pub struct ControlsPlugin;
+
+impl Plugin for ControlsPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<Direction>()
+            .add_system(user_input);
+    }
+}
+
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+use Direction::*;
+
+impl Default for Direction {
+    fn default() -> Self {
+        Right
+    }
+}
+
+fn user_input(
+    input: Res<Input<KeyCode>>,
+    mut last_pressed: ResMut<Direction>,
+) {
+    if input.pressed(KeyCode::Up) {
+        *last_pressed = Up;
+    } else if input.pressed(KeyCode::Down) {
+        *last_pressed = Down;
+    } else if input.pressed(KeyCode::Left) {
+        *last_pressed = Left;
+    } else if input.pressed(KeyCode::Right) {
+        *last_pressed = Right;
+    }
+}
